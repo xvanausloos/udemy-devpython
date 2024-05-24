99f1298879d17f76c681de9093906d459d9e2780
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2024-05-24 09:38:04 +0200

    module 145

--- module145/module145.py
+++ module145/module145.py
@@ -1,12 +1,14 @@
 # -- definition
 class Personne:
     # default constructor
-    def __init__(self):
-        print("constructeur personne")
-
+    def __init__(self, nom):
+        self.nom = nom
+        print(f"Bonjour mon nom est {nom}")
 
 
 if __name__ == '__main__':
     # implementation
-    personne1 = Personne()
-    personne2 = Personne()
+    personne1 = Personne("jean")
+    print(id(personne1))
+    personne2 = Personne("xav")
+    print(id(personne2))
