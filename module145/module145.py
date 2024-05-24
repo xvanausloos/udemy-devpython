0010ad82e383f09b1d635c5ebd38bc1eeef4b7b1
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2024-05-24 09:32:16 +0200

    add module145.py

---
+++ module145/module145.py
@@ -0,0 +1,12 @@
+# -- definition
+class Personne:
+    # default constructor
+    def __init__(self):
+        print("constructeur personne")
+
+
+
+if __name__ == '__main__':
+    # implementation
+    personne1 = Personne()
+    personne2 = Personne()
