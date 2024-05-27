d93bd7b93f6433d8376e26f9d324f439a9354910
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2024-05-27 13:30:09 +0200

    exercice m145 added age

--- module145/module145.py
+++ module145/module145.py
@@ -1,18 +1,19 @@
 # -- definition
 class Personne:
     # default constructor
-    def __init__(self, nom):
+    def __init__(self, nom: str, age: int):
         self.nom = nom
-        print(f"Bonjour mon nom est {nom}")
+        self.age = age
+        print(f"Bonjour mon nom est {nom} j'ai {age}")
 
     def sepresenter(self):
         print(f"Bonjour je m'appelle {self.nom}")
 
 if __name__ == '__main__':
     # implementation
-    personne1 = Personne("jean")
+    personne1 = Personne("jean", 10)
     print(id(personne1))
-    personne2 = Personne("xav")
+    personne2 = Personne("xav", 50)
     print(id(personne2))
     personne1.sepresenter()
     personne2.sepresenter()
