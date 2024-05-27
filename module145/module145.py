353ebd0b7877c60642989b59a4828a9dd79675a8
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2024-05-27 14:45:46 +0200

    exercice 148

--- module145/module145.py
+++ module145/module145.py
@@ -1,20 +1,30 @@
 # -- definition
 class Personne:
     # default constructor
-    def __init__(self, nom: str, age: int):
+    def __init__(self, nom: str = "", age: int = 0):
         self.nom = nom
+        if self.nom == "":
+            name = input("Quel est le nom de l'utilisateur ? ")
+            self.nom = name
         self.age = age
 
     def se_presenter(self):
-        print(f"Bonjour je m'appelle {self.nom}, j'ai {self.age}")
-        if self.est_majeur():
-            print(f"Je suis majeure")
-        else:
-            print(f"Je suis mineur")
+        info_personne =   f"Bonjour je m'appelle {self.nom}"
+        if self.age != 0:
+            info_personne += ", jai " + str(str(self.age) + " ans")
+        print(info_personne)
+
+        if self.age != 0:
+            if self.est_majeur():
+                print(f"Je suis majeure")
+            else:
+                print(f"Je suis mineur")
 
     def est_majeur(self) -> bool:
         return self.age >= 18
 
+    def demander_lenom(self):
+        pass
 
 if __name__ == '__main__':
     # implementation
@@ -24,4 +34,6 @@ if __name__ == '__main__':
     print(id(personne2))
     personne1.se_presenter()
     personne2.se_presenter()
+    personne3 = Personne()
+    personne3.se_presenter()
 
